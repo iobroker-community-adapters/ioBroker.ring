530a30b3baf7a86c599655bc8274a566fa9ac9b5
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-12-09 06:54:39 +0100

    First Version

---
+++ README.md
@@ -0,0 +1,46 @@
+![Logo](admin/ring.png)
+
+# Ring Video Doorbell Adapter
+
+
+[![Travis CI Build Status](https://travis-ci.org/schmupu/ioBroker.ring.svg?branch=master)](https://travis-ci.org/schmupu/ioBroker.ring)
+[![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/schmupu/ioBroker.ring?branch=master&svg=true)](https://ci.appveyor.com/project/schmupu/ioBroker-ring/)
+[![NPM version](http://img.shields.io/npm/v/ioBroker.ring.svg)](https://www.npmjs.com/package/ioBroker.ring)
+[![Downloads](https://img.shields.io/npm/dm/ioBroker.ring.svg)](https://www.npmjs.com/package/ioBroker.ring)
+
+[![NPM](https://nodei.co/npm/ioBroker.ring.png?downloads=true)](https://nodei.co/npm/ioBroker.ring/)
+
+...
+
+## Install & Configuration
+
+...
+
+## Changelog
+
+### 0.0.1 (07.12.2018)
+* (Stübi) First Version
+
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2018 Thorsten <thorsten@stueben.de> / <https://github.com/schmupu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ ring.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+let adapter = new utils.Adapter('ring');
+
+function decrypt(key, value) {
+  let result = '';
+  for (let i = 0; i < value.length; ++i) {
+    result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
+  }
+  return result;
+}
+
+
+
+
+// *****************************************************************************************************
+// is called when adapter shuts down - callback has to be called under any circumstances!
+// *****************************************************************************************************
+adapter.on('unload', (callback) => {
+  try {
+    adapter.log.info('Closing Adapter');
+    callback();
+  } catch (e) {
+    // adapter.log.error('Error');
+    callback();
+  }
+});
+
+
+// *****************************************************************************************************
+// Listen for sendTo messages
+// *****************************************************************************************************
+adapter.on('message', (msg) => {
+
+  adapter.sendTo(msg.from, msg.command, "Execute command " + msg.command, msg.callback);
+
+});
+
+
+// *****************************************************************************************************
+// is called when databases are connected and adapter received configuration.
+// start here!
+// *****************************************************************************************************
+adapter.on('ready', () => {
+
+  adapter.getForeignObject('system.config', (err, obj) => {
+    if (adapter.config.password) {
+      if (obj && obj.native && obj.native.secret) {
+        //noinspection JSUnresolvedVariable
+        adapter.config.password = decrypt(obj.native.secret, adapter.config.password);
+      } else {
+        //noinspection JSUnresolvedVariable
+        adapter.config.password = decrypt('Zgfr56gFe87jJOM', adapter.config.password);
+      }
+    }
+    main();
+  });
+
+});
+
+
+// *****************************************************************************************************
+// Main
+// *****************************************************************************************************
+function main() {
+
+
+}
