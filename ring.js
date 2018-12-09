7acb2fed65cf92abc8fac18b975d5952d4d9f6b8
Thorsten Stueben <thorsten.stueben@macbook.local>
2018-12-09 22:29:37 +0100

    Update

--- ring.js
+++ ring.js
@@ -2,6 +2,7 @@
 
 const utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 let adapter = new utils.Adapter('ring');
+let Doorbell2 = require(__dirname + '/lib/doorbell2');
 let Doorbell = require(__dirname + '/lib/doorbell');
 
 
@@ -67,7 +68,42 @@ adapter.on('ready', () => {
 // *****************************************************************************************************
 function main() {
 
-  let ring  = new Doorbell(adapter);
-  ring.test();
+  //let ring2  = new Doorbell2(adapter);
+  //ring2.test();
+
+  let ring = new Doorbell(adapter);
+  let events = ring.getEvent();
+
+  let dingdong = ring.dingDong();
+  dingdong();
+  events.on('dingdong', (ding) => {
+    adapter.log.info(JSON.stringify(ding));
+  });
+
+  /*
+  ring.getLiveStream()().then((urls) => {
+    adapter.log.info(JSON.stringify(urls));
+  }).catch((error) => {
+  });
+
+  ring.getHealthSummarie()().then((healths) => {
+    adapter.log.info(JSON.stringify(healths));
+  }).catch((error) => {
+  });
+  */
+
+  let actions = [
+    () => ring.getLiveStream()(),
+    () => ring.getHealthSummarie()(),
+    () => ring.getHistory()(),
+    () => ring.getLastVideos()() 
+    
+  ];
+  let promise = Promise.resolve();
+  let results = [];
+  for (let action of actions) {
+    promise = promise.then(action).then((r) => results.push(r));
+  }
+  promise.then(() => adapter.log.info("Done with results " + JSON.stringify(results)));
 
 }
